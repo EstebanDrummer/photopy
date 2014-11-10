2deeae42abd41b9a7a4c3047e7fa391e45a2b1e8
EstebanDrummer <yepesk8r@gmail.com>
2014-11-10 00:14:44 -0500

    Foto con información
    Se adiciona nueva vista de fotos con información, nombre del álbum,
    propietario, nombre de la foto.

--- PhotoBook/views.py
+++ PhotoBook/views.py
@@ -48,19 +48,39 @@ def privado(request):
 @login_required(login_url='/ingresar')
 def listaAlbum(request, var):
 	usuario = User.objects.get(username=var)
+	usuarioProp = request.user
 	pk = usuario.id
+	nombre = usuario.username
 	listaAlbum = Album.objects.filter(usuario_id=pk)
-	return render_to_response('listaAlbum.html',{'listaAlbum':listaAlbum}, context_instance=RequestContext(request))
+	return render_to_response('listaAlbum.html',{'nombreDuenio':nombre, 'usuario':usuarioProp, 'listaAlbum':listaAlbum}, context_instance=RequestContext(request))
 
 @login_required(login_url='/ingresar')
 def listaFoto(request, var):
 	album = Album.objects.get(nombre=var)
+	nombreAlbum=var
+	usuarioProp = request.user
+	duenio = User.objects.get(id=album.usuario_id)
+	nombre = duenio.username
 	pk = album.id
 	if request.user.has_perm('can_view_all') or (request.user.id==album.usuario_id):
 		listaFotos=Foto.objects.filter(album_id=pk)
 	else:
 		listaFotos=Foto.objects.filter(album_id=pk,es_publica='1')	
-	return render_to_response('listaFotos.html',{'listaFotos':listaFotos}, context_instance=RequestContext(request))
+	return render_to_response('listaFotos.html',{'usuario':usuarioProp, 'nombreDuenio':nombre,'listaFotos':listaFotos, 'nombreAlbum':nombreAlbum}, context_instance=RequestContext(request))
+
+@login_required(login_url='/ingresar')
+def fotoInfo(request, var):
+	foto=Foto.objects.get(nombre=var)
+	album_id= foto.album_id
+	album = Album.objects.get(id=album_id)
+	duenio = User.objects.get(id=album.usuario_id)
+	usuarioProp = request.user
+	pk = album.id
+	if (request.user.has_perm('can_view_all') or (request.user.id==album.usuario_id)) or (foto.es_publica==1):
+		fotoInfo=Foto.objects.get(id=foto.id)
+	else:
+		return HttpResponseRedirect('/ingresar')	
+	return render_to_response('FotoInfo.html',{'usuario':usuarioProp, 'fotoInfo':fotoInfo, 'album':album, 'duenio':duenio}, context_instance=RequestContext(request))
 
 
 @login_required(login_url='/ingresar')
